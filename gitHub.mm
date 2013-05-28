4c5a44de4a021d47ad14149cc98448eab03e96c1
zj <zhuj02@gmail.com>
2013-05-28 10:40:57 +0800

    just for test
    test

---
+++ gitHub.mm
@@ -0,0 +1,17 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1369704394546" ID="ID_1551772263" MODIFIED="1369704404062" TEXT="gitHub">
+<node CREATED="1369704405562" ID="ID_1953820162" MODIFIED="1369704413843" POSITION="right" TEXT="&#x4ecb;&#x7ecd;">
+<node CREATED="1369704414765" ID="ID_778317749" MODIFIED="1369704425921" TEXT="&#x5206;&#x5e03;&#x5f0f;&#x7248;&#x672c;&#x7ba1;&#x7406;&#x5de5;&#x5177;"/>
+<node CREATED="1369704514375" ID="ID_738518996" MODIFIED="1369704548484" TEXT="&#x6709;&#x514d;&#x8d39;&#x7684;&#x7f51;&#x7ad9;&#x670d;&#x52a1;-github"/>
+</node>
+<node CREATED="1369704580703" ID="ID_242271880" MODIFIED="1369704655734" POSITION="right" TEXT="&#x5b89;&#x88c5;&#xa;https://help.github.com/articles/set-up-git">
+<node CREATED="1369704585296" ID="ID_23523730" MODIFIED="1369706247500" TEXT="&#x76f4;&#x63a5;&#x5b89;&#x88c5;&#x5373;&#x53ef;"/>
+</node>
+<node CREATED="1369706252046" ID="ID_1604005668" MODIFIED="1369706311312" POSITION="right" TEXT="&#x4f7f;&#x7528;">
+<node CREATED="1369706255765" ID="ID_897762590" MODIFIED="1369706270671" TEXT="&#x5728;github&#x4e0a;&#x7533;&#x8bf7;&#x4e00;&#x4e2a;&#x65b0;&#x7684;&#x8d26;&#x6237;"/>
+<node CREATED="1369706270968" ID="ID_1253893833" MODIFIED="1369706369750" TEXT="&#x6253;&#x5f00;&#x672c;&#x673a;&#x7684;github&#xff0c;&#x5efa;&#x7acb;&#x672c;&#x5730;repository"/>
+<node CREATED="1369706371625" ID="ID_462588054" MODIFIED="1369706394562" TEXT="&#x8fdb;&#x884c;&#x4e0a;&#x4f20;&#x3002;&#xff08;&#x4e0d;&#x80fd;&#x901a;&#x8fc7;&#x4ee3;&#x7406;&#xff09;"/>
+</node>
+</node>
+</map>
